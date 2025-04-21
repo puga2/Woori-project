2bde48075da481e1fcd9c8098f273fbf3c8142be
GaGa <o.kimhong168@gmail.com>
2025-04-21 09:55:05 +0700

    first commit of project woori

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <nav className='bg-blue-600'>
+        <div className="container">
+            hi
+        </div>
+    </nav>
+  )
+}
+
+export default Navigation
