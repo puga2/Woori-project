9ab921ad9414b6e5546e5d6325cf6a1d7a2d038f
GaGa <o.kimhong168@gmail.com>
2025-05-01 16:54:59 +0700

    seciont info card

--- src/components/SectionInfo.jsx
+++ src/components/SectionInfo.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import sectinLeft from '../../src/assets/images/carousel/sectionLeft.jpg';
 const SectionInfo = () => {
   return (
    <section className=''>
@@ -7,6 +7,32 @@ const SectionInfo = () => {
             <h2 className='text-black text-5xl text-center my-18'>
             ធនាគារដែលផ្តល់សេវាហិរញ្ញវត្ថុផ្ទាល់ខ្លួន និងអាជីវកម្មជិត <br /> លោកអ្នកបំផុត
             </h2>
+            <div className='flex justify-between   gap-7 '>
+              <div className='bg-yellow-700 pt-11  pb-96 px-9 rounded-4xl overflow-hidden relative'>
+                <h3 className='text-4xl text-black font-bold mb-5'>
+                សេវាបុគ្គល
+                </h3>
+                <p className='text-xl text-black'>យើង គឺជាកន្លែងដែលនៅជិតលោកអ្នកបំផុតក្នុងការសន្សំ និងជួយឱ្យប្រាក់របស់អ្នករីកលូតលាស់ជាមួយនឹងសេវាធនាគាររហ័ស ងាយស្រួលប្រកបដោយទំនុកចិត្ត។</p>
+
+              <div className='absolute -bottom-9  left-0  overflow-hidden  rounded-se-full w-96 h-96'  style={{
+                backgroundImage: `url(${sectinLeft})`,
+                backgroundSize: "cover",
+                backgroundPosition: "left",
+              }}>
+                {/* <img src={sectinLeft} alt="" className='w-full h-full object-cover image' style={{ transform: "scaleX(-1) ", objectPosition: "10re right" }} /> */}
+              
+              </div>
+              </div>
+
+              <div className='bg-yellow-700 py-11 px-9 rounded-4xl'>
+                <h3 className='text-4xl text-black font-bold mb-5'>
+                សេវាបុគ្គល
+                </h3>
+                <p className='text-xl text-black'>យើង គឺជាកន្លែងដែលនៅជិតលោកអ្នកបំផុតក្នុងការសន្សំ និងជួយឱ្យប្រាក់របស់អ្នករីកលូតលាស់ជាមួយនឹងសេវាធនាគាររហ័ស ងាយស្រួលប្រកបដោយទំនុកចិត្ត។</p>
+
+              </div>
+            
+            </div>
         </div>
    </section>
   )
